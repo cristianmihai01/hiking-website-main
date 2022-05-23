dade2259b7123bcd36d45fe8506c19e7b7649ebe
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-05-23 23:08:31 +0300

    added

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const nav = document.querySelector('.nav');
 const navTrigger = document.querySelector('.header__nav-trigger');
+const header = document.querySelector('.header');
 
-// open / close menu
+// open - close menu
 navTrigger.addEventListener('click', () => {
   nav.classList.toggle('open');
+});
+
+// header background on scroll
+window.addEventListener('scroll', (e) => {
+  if (window.scrollY > 40) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
 });

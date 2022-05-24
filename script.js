c37a5920a0f0b10c1704b216de4465c47d39a4f5
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-05-24 22:28:14 +0300

    added

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ window.addEventListener('scroll', (e) => {
     header.classList.remove('active');
   }
 });
+
+// swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});

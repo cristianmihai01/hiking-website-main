9c1f54d94324f1f61447f60ea4f0b214cb33fc78
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-05-26 11:54:02 +0300

    added

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,26 @@ let swiper = new Swiper('.slider', {
     },
   },
 });
+
+// Scroll Reveal
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+sr.reveal(
+  '.hero__title, .companies__title, .companies__pretitle, .testimonials__title, .testimonials__pretitle, .features__container, .newsletter__container, .footer__container'
+);
+sr.reveal('.hero__subtitle', { delay: 500 });
+sr.reveal('.hero__btn', { delay: 600 });
+sr.reveal('.hero__img', { delay: 600, origin: 'bottom' });
+sr.reveal('.feature', { interval: '100' });
+sr.reveal('.features__explore-text', { delay: 600, origin: 'bottom' });
+sr.reveal('.section-primary__img', { origin: 'left' });
+sr.reveal('.section-primary__text', { origin: 'right' });
+sr.reveal('.brands__item', { interval: '100' });
+sr.reveal('.slider', { interval: '100' });
+sr.reveal('.footer__copyright', { delay: 200, origin: 'top' });
